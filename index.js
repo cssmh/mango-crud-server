9e25f8c6951febc865ccac9894b0908fe299dca8
momen210 <touristmomen@gmail.com>
2024-02-07 16:52:18 +0600

    sending data to database is working

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// mominitmbstu
-// BARJt1FPafkaqhFF
-
 // Mongo code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ae0fypv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const mangoCollection = client.db("MangoDB").collection("mangoes");
+
+    app.post("/mangoes", async(req, res) => {
+      const gotMangoesFromCLient = req.body;
+      console.log(gotMangoesFromCLient);
+      const result = await mangoCollection.insertOne(gotMangoesFromCLient)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +44,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
