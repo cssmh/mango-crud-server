630b5f1d64edc5f8ac33df71e3ffb6085bc49aab
momen210 <touristmomen@gmail.com>
2024-02-07 18:58:50 +0600

    completed task, can update mango info

--- index.js
+++ index.js
@@ -28,19 +28,54 @@ async function run() {
 
     const mangoCollection = client.db("MangoDB").collection("mangoes");
 
+    // create new
     app.post("/mangoes", async (req, res) => {
       const gotMangoesFromCLient = req.body;
       console.log(gotMangoesFromCLient);
       const result = await mangoCollection.insertOne(gotMangoesFromCLient);
       res.send(result);
     });
+    // create new end
 
+    // get and show all
     app.get("/mangos", async (req, res) => {
       const cursor = mangoCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    // get and show all end
 
+    // update part here
+    // first get that id to send to update
+    app.get("/update/:id", async (req, res) => {
+      const gotParamsId = req.params.id;
+      const query = { _id: new ObjectId(gotParamsId) };
+      const result = await mangoCollection.findOne(query);
+      res.send(result);
+    });
+    // first get that id to send to update end
+    // set that id, time to update to database
+    app.put("/update/:id", async (req, res) => {
+      const paramsId = req.params.id;
+      const updatedInfoFromClient = req.body;
+      const filter = { _id: new ObjectId(paramsId) };
+      const options = { upsert: true };
+      const finalUpdateMangoes = {
+        $set: {
+          name: updatedInfoFromClient.name,
+          photo: updatedInfoFromClient.photo,
+        },
+      };
+      const result = await mangoCollection.updateOne(
+        filter,
+        finalUpdateMangoes,
+        options
+      );
+      res.send(result);
+    });
+    // update part here end
+
+    // delete a single one
     app.delete("/mangoes/:id", async (req, res) => {
       const deleteId = req.params.id;
       console.log("got id from client", deleteId);
@@ -48,6 +83,7 @@ async function run() {
       const result = await mangoCollection.deleteOne(query);
       res.send(result);
     });
+    // delete a single one end
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
